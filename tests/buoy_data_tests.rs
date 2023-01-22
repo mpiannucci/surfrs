609d198888c891e46a68e11a5e180ae8e9b2a032
Matthew Iannucci <mpiannucci@gmail.com>
2023-01-22 12:24:50 -0500

    waimea overlfow exmaple

--- tests/buoy_data_tests.rs
+++ tests/buoy_data_tests.rs
@@ -2,13 +2,17 @@ extern crate surfrs;
 
 use std::f64::consts::PI;
 use std::fs;
-use surfrs::data::forecast_cbulletin_wave_data_record::{ForecastCBulletinWaveRecordCollection, ForecastCBulletinWaveRecord};
-use surfrs::data::forecast_spectral_wave_data_record::{ForecastSpectralWaveDataRecordCollection};
+use surfrs::data::forecast_cbulletin_wave_data_record::{
+    ForecastCBulletinWaveRecord, ForecastCBulletinWaveRecordCollection,
+};
+use surfrs::data::forecast_spectral_wave_data_record::ForecastSpectralWaveDataRecordCollection;
 use surfrs::data::latest_obs_data_record::LatestObsDataRecordCollection;
 use surfrs::data::meteorological_data_record::MeteorologicalDataRecordCollection;
-use surfrs::data::spectral_wave_data_record::{SpectralWaveDataRecordCollection, DirectionalSpectralWaveDataRecord};
-use surfrs::data::wave_data_record::{WaveDataRecordCollection};
-use surfrs::swell::{SwellProvider};
+use surfrs::data::spectral_wave_data_record::{
+    DirectionalSpectralWaveDataRecord, SpectralWaveDataRecordCollection,
+};
+use surfrs::data::wave_data_record::WaveDataRecordCollection;
+use surfrs::swell::SwellProvider;
 use surfrs::tools::vector::bin;
 use surfrs::units::{UnitConvertible, Units};
 
@@ -21,7 +25,7 @@ fn read_latest_obs_data() {
     let raw_data = read_mock_data("latest_obs.txt");
     let mut data_collection = LatestObsDataRecordCollection::from_data(raw_data.as_str());
     let mut data_records = data_collection.records();
-    
+
     // assert_eq!(data_records.count(), 865);
 
     let nantucket = data_records.find(|s| s.station_id == "44097");
@@ -36,7 +40,7 @@ fn read_meteorological_data() {
     let raw_data = read_mock_data("44017.met.txt");
     let mut data_collection = MeteorologicalDataRecordCollection::from_data(raw_data.as_str());
     let data_records = data_collection.records();
-    
+
     assert_eq!(data_records.count(), 1099);
 }
 
@@ -54,7 +58,7 @@ fn read_wave_energy_data() {
     let raw_data = read_mock_data("44008.data_spec");
     let mut data_collection = SpectralWaveDataRecordCollection::from_data(raw_data.as_str());
     let records = data_collection.records();
-    
+
     assert!(records.count() == 1084);
 }
 
@@ -69,36 +73,39 @@ fn read_wave_direction_data() {
 
 #[test]
 fn read_wave_spectra_data() {
-    // let raw_energy_data = read_mock_data("44097.data_spec");
-    // let raw_mean_wave_direction_data = read_mock_data("44097.swdir");
-    // let raw_primary_wave_direction_data = read_mock_data("44097.swdir2");
-    // let raw_first_polar_coefficient_data = read_mock_data("44097.swr1");
-    // let raw_second_polar_coefficient_data = read_mock_data("44097.swr2");
-
     let raw_energy_data = read_mock_data("44097.data_spec");
     let raw_mean_wave_direction_data = read_mock_data("44097.swdir");
     let raw_primary_wave_direction_data = read_mock_data("44097.swdir2");
     let raw_first_polar_coefficient_data = read_mock_data("44097.swr1");
     let raw_second_polar_coefficient_data = read_mock_data("44097.swr2");
 
-    let mut energy_data_collection = SpectralWaveDataRecordCollection::from_data(raw_energy_data.as_str());
-    let mut mean_wave_direction_data_collection = SpectralWaveDataRecordCollection::from_data(&raw_mean_wave_direction_data.as_str());
-    let mut primary_wave_direction_data_collection = SpectralWaveDataRecordCollection::from_data(&raw_primary_wave_direction_data.as_str());
-    let mut first_polar_coefficient_collection = SpectralWaveDataRecordCollection::from_data(&raw_first_polar_coefficient_data.as_str());
-    let mut second_polar_coefficient_collection = SpectralWaveDataRecordCollection::from_data(&raw_second_polar_coefficient_data.as_str());
-    
+    let mut energy_data_collection =
+        SpectralWaveDataRecordCollection::from_data(raw_energy_data.as_str());
+    let mut mean_wave_direction_data_collection =
+        SpectralWaveDataRecordCollection::from_data(&raw_mean_wave_direction_data.as_str());
+    let mut primary_wave_direction_data_collection =
+        SpectralWaveDataRecordCollection::from_data(&raw_primary_wave_direction_data.as_str());
+    let mut first_polar_coefficient_collection =
+        SpectralWaveDataRecordCollection::from_data(&raw_first_polar_coefficient_data.as_str());
+    let mut second_polar_coefficient_collection =
+        SpectralWaveDataRecordCollection::from_data(&raw_second_polar_coefficient_data.as_str());
+
     let dir_count = 36usize;
     let dir_step = (2.0 * PI) / dir_count as f64;
-    let directions = (0..dir_count).map(|i| dir_step * (i as f64)).collect::<Vec<f64>>();
+    let directions = (0..dir_count)
+        .map(|i| dir_step * (i as f64))
+        .collect::<Vec<f64>>();
 
     let records = itertools::izip!(
-        energy_data_collection.records(), 
-        mean_wave_direction_data_collection.records(), 
-        primary_wave_direction_data_collection.records(), 
-        first_polar_coefficient_collection.records(), 
+        energy_data_collection.records(),
+        mean_wave_direction_data_collection.records(),
+        primary_wave_direction_data_collection.records(),
+        first_polar_coefficient_collection.records(),
         second_polar_coefficient_collection.records(),
     )
-        .map(|(e, mwd, pwd, r1, r2)| DirectionalSpectralWaveDataRecord::from_data(&directions, e, mwd, pwd, r1, r2));
+    .map(|(e, mwd, pwd, r1, r2)| {
+        DirectionalSpectralWaveDataRecord::from_data(&directions, e, mwd, pwd, r1, r2)
+    });
 
     let record = records.skip(6).next().unwrap();
 
@@ -124,11 +131,20 @@ fn read_wave_spectra_data() {
     // let control = "0.7 m @ 4.5 s 168° sse, 0.6 m @ 12.5 s 120° ese, 0.6 m @ 10.5 s 112° ese, 0.5 m @ 3.8 s 160° sse";
     // let out = swell_components.join(", ");
 
-    swell_data.components.iter_mut().enumerate().for_each(|(i, component)| {
-        let is_false_positive = false_components.contains(&i);
-        component.to_units(&Units::English);
-        println!("BUOY -- {} {} {}", &component, component.energy.clone().unwrap(), !is_false_positive);
-    });
+    swell_data
+        .components
+        .iter_mut()
+        .enumerate()
+        .for_each(|(i, component)| {
+            let is_false_positive = false_components.contains(&i);
+            component.to_units(&Units::English);
+            println!(
+                "BUOY -- {} {} {}",
+                &component,
+                component.energy.clone().unwrap(),
+                !is_false_positive
+            );
+        });
 
     println!("buoy dirs: {:?}", &record.spectra.direction_deg());
     let cart_e = record.spectra.project_cartesian(50, Some(25.0), None);
@@ -138,7 +154,10 @@ fn read_wave_spectra_data() {
 
     println!("buoy mwd:: {:?}", &record.spectra.mean_wave_direction_f());
 
-    let _ = fs::write("contours2.json", &record.spectra.contoured().unwrap().to_string());
+    let _ = fs::write(
+        "contours2.json",
+        &record.spectra.contoured().unwrap().to_string(),
+    );
 }
 
 #[test]
@@ -148,7 +167,8 @@ fn read_cbulletin_forecast_station_data() {
     let bulletin_records_iter = data_collection.records();
     assert!(bulletin_records_iter.is_ok());
 
-    let bulletin_records: Vec<ForecastCBulletinWaveRecord> = bulletin_records_iter.unwrap().1.collect();
+    let bulletin_records: Vec<ForecastCBulletinWaveRecord> =
+        bulletin_records_iter.unwrap().1.collect();
     assert!(bulletin_records[0].swell_data().is_ok());
     for (_, record) in bulletin_records.iter().enumerate() {
         assert!(record.swell_data().is_ok());
@@ -158,7 +178,8 @@ fn read_cbulletin_forecast_station_data() {
 #[test]
 fn read_spectral_forecast_station_data() {
     let raw_data = read_mock_data("gfswave.44097.spec");
-    let mut data_collection = ForecastSpectralWaveDataRecordCollection::from_data(raw_data.as_str());
+    let mut data_collection =
+        ForecastSpectralWaveDataRecordCollection::from_data(raw_data.as_str());
     let spectral_records_iter = data_collection.records();
     assert!(spectral_records_iter.is_ok());
 
@@ -186,13 +207,67 @@ fn read_spectral_forecast_station_data() {
 
     for mut component in swell_data.components {
         component.to_units(&Units::English);
-        println!("FORECAST -- {} {}", component.clone(), component.energy.unwrap());
+        println!(
+            "FORECAST -- {} {}",
+            component.clone(),
+            component.energy.unwrap()
+        );
     }
 
     println!("forecast dirs: {:?}", record.spectra.direction_deg());
-    println!("forecast cartesian: {:?}", record.spectra.project_cartesian(50, Some(25.0), None));
+    println!(
+        "forecast cartesian: {:?}",
+        record.spectra.project_cartesian(50, Some(25.0), None)
+    );
 
-    println!("forecast mwd:: {:?}", &record.spectra.mean_wave_direction_f());
+    println!(
+        "forecast mwd:: {:?}",
+        &record.spectra.mean_wave_direction_f()
+    );
 
-    let _ = fs::write("contours.json", &record.spectra.contoured().unwrap().to_string());
+    let _ = fs::write(
+        "contours.json",
+        &record.spectra.contoured().unwrap().to_string(),
+    );
+}
+
+#[test]
+fn read_waimea_spectra_data() {
+    let raw_energy_data = read_mock_data("waimea_overflow/51201.data_spec");
+    let raw_mean_wave_direction_data = read_mock_data("waimea_overflow/51201.swdir");
+    let raw_primary_wave_direction_data = read_mock_data("waimea_overflow/51201.swdir2");
+    let raw_first_polar_coefficient_data = read_mock_data("waimea_overflow/51201.swr1");
+    let raw_second_polar_coefficient_data = read_mock_data("waimea_overflow/51201.swr2");
+
+    let mut energy_data_collection =
+        SpectralWaveDataRecordCollection::from_data(raw_energy_data.as_str());
+    let mut mean_wave_direction_data_collection =
+        SpectralWaveDataRecordCollection::from_data(&raw_mean_wave_direction_data.as_str());
+    let mut primary_wave_direction_data_collection =
+        SpectralWaveDataRecordCollection::from_data(&raw_primary_wave_direction_data.as_str());
+    let mut first_polar_coefficient_collection =
+        SpectralWaveDataRecordCollection::from_data(&raw_first_polar_coefficient_data.as_str());
+    let mut second_polar_coefficient_collection =
+        SpectralWaveDataRecordCollection::from_data(&raw_second_polar_coefficient_data.as_str());
+
+    let dir_count = 36usize;
+    let dir_step = (2.0 * PI) / dir_count as f64;
+    let directions = (0..dir_count)
+        .map(|i| dir_step * (i as f64))
+        .collect::<Vec<f64>>();
+
+    let mut records = itertools::izip!(
+        energy_data_collection.records(),
+        mean_wave_direction_data_collection.records(),
+        primary_wave_direction_data_collection.records(),
+        first_polar_coefficient_collection.records(),
+        second_polar_coefficient_collection.records(),
+    )
+    .map(|(e, mwd, pwd, r1, r2)| {
+        DirectionalSpectralWaveDataRecord::from_data(&directions, e, mwd, pwd, r1, r2)
+    });
+
+    let record = records.skip(3).next().unwrap();
+    let swell_data = record.swell_data().unwrap(); 
+    println!("{}", swell_data.summary);
 }
