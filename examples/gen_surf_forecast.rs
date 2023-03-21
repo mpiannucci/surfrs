07d0af51f935b782daa071edb259e9ff058a7164
Matthew Iannucci <mpiannucci@gmail.com>
2023-03-21 18:29:33 -0400

    Ready for weather data merhging with surf forecast

--- examples/gen_surf_forecast.rs
+++ examples/gen_surf_forecast.rs
@@ -1,4 +1,5 @@
 use std::{fs, future};
+use std::time::Instant;
 
 use chrono::{DateTime, Utc};
 use futures_util::future::try_join_all;
@@ -47,78 +48,84 @@ impl UnitConvertible<SurfForecastDataRecord> for SurfForecastDataRecord {
 
 #[tokio::main]
 async fn main() {
-    // let location = Location::new(41.35, -71.4, "Block Island Sound".into());
-    // let depth = 30.0;
-    // let angle =  145.0;
-    // let slope = 0.02;
-    // let now = chrono::Utc::now();
+    let start = Instant::now();
 
-    // println!("Fetching GFS Wave Model Data");
+    let location = Location::new(41.35, -71.4, "Block Island Sound".into());
+    let depth = 30.0;
+    let angle =  145.0;
+    let slope = 0.02;
+    let now = chrono::Utc::now();
 
-    // let atlantic_wave_model = GFSWaveModel::atlantic();
+    println!("Fetching GFS Wave Model Data");
 
-    // let client = Client::new();
-    // let requests = (0..120)
-    //     .map(|i| {
-    //         let url = atlantic_wave_model.create_url(&ModelDataSource::NODDAWS, i, Some(now));
-    //         let client = &client;
-    //         async move {
-    //             let resp = client.get(url).send().await?;
-    //             resp.bytes().await
-    //         }
-    //     });
+    let atlantic_wave_model = GFSWaveModel::atlantic();
 
-    // let data = try_join_all(requests)
-    //     .await
-    //     .unwrap()
-    //     .iter()
-    //     .map(|b| {
-    //         // Extract data to grib data records
-    //         let messages = read_messages(b).collect();
-    //         let record = GFSWaveGribPointDataRecord::from_messages(&atlantic_wave_model, &messages, &location);
+    let client = Client::new();
+    let requests = (0..120)
+        .map(|i| {
+            let url = atlantic_wave_model.create_url(&ModelDataSource::NODDAWS, i, Some(now));
+            let client = &client;
+            async move {
+                let resp = client.get(url).send().await?;
+                resp.bytes().await
+            }
+        });
 
-    //         // Compute breaking wave data
-    //         let breaking_wave_heights = record.swell_components
-    //             .iter()
-    //             .filter_map(|s| estimate_breaking_wave_height(s, angle, slope, depth).ok())
-    //             .collect::<Vec<_>>();
+    let data = try_join_all(requests)
+        .await
+        .unwrap()
+        .iter()
+        .enumerate()
+        .map(|(i, b)| {
+            println!("Processing GFS Wave Model Data: {} of 120", i + 1);
 
-    //         // https://github.com/mpiannucci/surfpy/blob/af65f70c36c37b3454305711058cabc15d129028/surfpy/swell.py#L42
-    //         let (_, breaking_wave_height) = min_max(&breaking_wave_heights);
+            // Extract data to grib data records
+            let messages = read_messages(b).collect();
+            let record = GFSWaveGribPointDataRecord::from_messages(&atlantic_wave_model, &messages, &location);
 
-    //         // Take the maximum breaking height and give it a scale factor of 0.9 for refraction
-    //         // or anything we are not checking for.
-    //         let breaking_wave_height = breaking_wave_height * 0.8;
-    //         let max_breaking_wave_height = DimensionalData {
-    //             value: Some(breaking_wave_height),
-    //             variable_name: "max reaking wave height".into(),
-    //             unit: Unit::Meters
-    //         };
+            // Compute breaking wave data
+            let breaking_wave_heights = record.swell_components
+                .iter()
+                .filter_map(|s| estimate_breaking_wave_height(s, angle, slope, depth).ok())
+                .collect::<Vec<_>>();
 
-    //         // For now assume this is significant wave height as the max and the rms as the min
-    //         let min_breaking_wave_height = DimensionalData {
-    //             value: Some(breaking_wave_height / 1.4),
-    //             variable_name: "min breaking wave height".into(),
-    //             unit: Unit::Meters
-    //         };
+            // https://github.com/mpiannucci/surfpy/blob/af65f70c36c37b3454305711058cabc15d129028/surfpy/swell.py#L42
+            let (_, breaking_wave_height) = min_max(&breaking_wave_heights);
 
-    //         let mut record = SurfForecastDataRecord {
-    //             date: record.date,
-    //             wave_summary: record.wave_summary,
-    //             wind_speed: record.wind_speed,
-    //             wind_direction: record.wind_direction,
-    //             swell_components: record.swell_components,
-    //             minimum_breaking_height: min_breaking_wave_height,
-    //             maximum_breaking_height: max_breaking_wave_height,
-    //         };
+            // Take the maximum breaking height and give it a scale factor of 0.9 for refraction
+            // or anything we are not checking for.
+            let breaking_wave_height = breaking_wave_height * 0.8;
+            let max_breaking_wave_height = DimensionalData {
+                value: Some(breaking_wave_height),
+                variable_name: "max reaking wave height".into(),
+                unit: Unit::Meters
+            };
 
-    //         record.to_units(&UnitSystem::English);
+            // For now assume this is significant wave height as the max and the rms as the min
+            let min_breaking_wave_height = DimensionalData {
+                value: Some(breaking_wave_height / 1.4),
+                variable_name: "min breaking wave height".into(),
+                unit: Unit::Meters
+            };
 
-    //         record
-    //     })
-    //     .collect::<Vec<_>>();
+            let mut record = SurfForecastDataRecord {
+                date: record.date,
+                wave_summary: record.wave_summary,
+                wind_speed: record.wind_speed,
+                wind_direction: record.wind_direction,
+                swell_components: record.swell_components,
+                minimum_breaking_height: min_breaking_wave_height,
+                maximum_breaking_height: max_breaking_wave_height,
+            };
+
+            record.to_units(&UnitSystem::English);
+
+            record
+        })
+        .collect::<Vec<_>>();
 
     // Fetch weather forecast
+    println!("Fetching NWS Hourly Weather Forecast");
 
     let client = reqwest::Client::builder()
         .user_agent("hopewaves.app")
@@ -144,12 +151,16 @@ async fn main() {
         .unwrap()
         .json::<NwsWeatherForecastDataRecordCollection>()
         .await
-        .unwrap();
+        .unwrap()
+        .records();
 
-    println!("{:#?}", weather_forecast);
+    println!("TODO: Merging Weather and Wave Data");
 
     // Combine and export json forecast data
 
+    println!("TODO: Writing Surf Forecast Data");
     // let data = serde_json::to_string(&data).unwrap();
     // fs::write("forecast.json", data).unwrap();
+
+    println!("Finished Surf Forecast Generation in {} seconds", start.elapsed().as_secs());
 }
